c0c1dd95240cfd7ff6aea755996fe0878bd1750e
Nasyrov Evgenii <inasyrov@ya.ru>
2018-01-14 16:36:04 +0000

    Register the indicator manager

--- src/IndicatorServiceProvider.php
+++ src/IndicatorServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Laratrade\Indicators;
 
 use Illuminate\Support\ServiceProvider;
+use Laratrade\Indicators\Contracts\IndicatorManager as IndicatorManagerContract;
 
 class IndicatorServiceProvider extends ServiceProvider
 {
@@ -18,7 +19,47 @@ class IndicatorServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(IndicatorManagerContract::class, function () {
+            return tap(new IndicatorManager, function ($manager) {
+                $this->registerIndicators($manager);
+            });
+        });
+    }
+
+    /**
+     * Register the indicators on the manager.
+     *
+     * @param IndicatorManagerContract $manager
+     */
+    protected function registerIndicators(IndicatorManagerContract $manager)
+    {
+        foreach (['Ao', 'Cmo'] as $indicator) {
+            $this->{"register{$indicator}Indicator"}($manager);
+        }
+    }
+
+    /**
+     * Register the awesome oscillator indicator.
+     *
+     * @param IndicatorManagerContract $manager
+     */
+    protected function registerAoIndicator(IndicatorManagerContract $manager)
+    {
+        $manager->add('ao', function () {
+            return new AwesomeOscillatorIndicator;
+        });
+    }
+
+    /**
+     * Register the change momentum oscillator indicator.
+     *
+     * @param IndicatorManagerContract $manager
+     */
+    protected function registerCmoIndicator(IndicatorManagerContract $manager)
+    {
+        $manager->add('cmo', function () {
+            return new ChangeMomentumOscillatorIndicator;
+        });
     }
 
     /**
@@ -29,6 +70,7 @@ class IndicatorServiceProvider extends ServiceProvider
     public function provides()
     {
         return [
+            IndicatorManagerContract::class,
         ];
     }
 }
