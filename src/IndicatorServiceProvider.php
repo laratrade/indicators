87ced8b20318cb7ebaf11649166b41df8e26cea0
Nasyrov Evgenii <inasyrov@ya.ru>
2018-01-14 15:35:00 +0000

    Add the boilerplate service provider

---
+++ src/IndicatorServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Laratrade\Indicators;
+
+use Illuminate\Support\ServiceProvider;
+
+class IndicatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+        ];
+    }
+}
