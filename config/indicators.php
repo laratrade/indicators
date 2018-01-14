e48a2b03a212c83fd66e5180b47fd91df2c533a0
Nasyrov Evgenii <inasyrov@ya.ru>
2018-01-14 23:23:49 +0000

    Add the indicators config

---
+++ config/indicators.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'admi'      => \Laratrade\Indicators\AverageDirectionalMovementIndexIndicator::class,
+    'atr'       => \Laratrade\Indicators\AverageTrueRangeIndicator::class,
+    'ao'        => \Laratrade\Indicators\AwesomeOscillatorIndicator::class,
+    'bb'        => \Laratrade\Indicators\BollingerBandsIndicator::class,
+    'cmo'       => \Laratrade\Indicators\ChangeMomentumOscillatorIndicator::class,
+    'cci'       => \Laratrade\Indicators\CommodityChannelIndexIndicator::class,
+    'htit'      => \Laratrade\Indicators\HilbertTransformInstantaneousTrendlineIndicator::class,
+    'hts'       => \Laratrade\Indicators\HilbertTransformSinewaveIndicator::class,
+    'httvcm'    => \Laratrade\Indicators\HilbertTransformTrendVersusCycleModeIndicator::class,
+    'mmi'       => \Laratrade\Indicators\MarketMeannessIndexIndicator::class,
+    'mfi'       => \Laratrade\Indicators\MoneyFlowIndexIndicator::class,
+    'macd'      => \Laratrade\Indicators\MovingAverageCrossoverDivergenceIndicator::class,
+    'macdwcmat' => \Laratrade\Indicators\MovingAverageCrossoverDivergenceWithControllableMovingAverageTypeIndicator::class,
+    'obv'       => \Laratrade\Indicators\OnBalanceVolumeIndicator::class,
+
+];
