4dfed382413b0c46b0219deb965728f92b827b2f
Nasyrov Evgenii <inasyrov@ya.ru>
2018-01-14 16:57:27 +0000

    Add the indicator service provider test

--- tests/Integration/IndicatorServiceProviderTest.php
+++ tests/Integration/IndicatorServiceProviderTest.php
@@ -2,11 +2,21 @@
 
 namespace Laratrade\Indicators\Tests\Integration;
 
+use Laratrade\Indicators\Contracts\IndicatorManager as IndicatorManagerContract;
+use Laratrade\Indicators\IndicatorManager;
+
 class IndicatorServiceProviderTest extends TestCase
 {
     /** @test */
     public function it_registers_service_provider()
     {
-        //
+        $this->assertTrue(
+            $this->app->bound(IndicatorManagerContract::class)
+        );
+
+        $this->assertInstanceOf(
+            IndicatorManager::class,
+            $this->app->make(IndicatorManagerContract::class)
+        );
     }
 }
