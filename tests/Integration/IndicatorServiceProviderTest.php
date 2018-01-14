3f53b7c1dadf74628f3c22e41920cb92c300bf20
Nasyrov Evgenii <inasyrov@ya.ru>
2018-01-14 15:37:49 +0000

    Add the indicator service provider test

---
+++ tests/Integration/IndicatorServiceProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Laratrade\Indicators\Tests\Integration;
+
+class IndicatorServiceProviderTest extends TestCase
+{
+    /** @test */
+    public function it_registers_service_provider()
+    {
+        //
+    }
+}
